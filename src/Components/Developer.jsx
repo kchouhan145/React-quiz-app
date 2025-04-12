1235510fd6f58669a12c4f8fbf359c03b35c38b1
kchouhan145 <k4studies@gmail.com>
2025-04-12 21:40:57 +0530

    Update Developer.jsx

--- src/Components/Developer.jsx
+++ src/Components/Developer.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Developer = () => {
   return (
-    <div className='bg-[#D0D2D4] h-full flex justify-center items-center dark:bg-slate-800'>
+    <div className='bg-[#D0D2D4] h-[100vh] flex justify-center items-center dark:bg-slate-800'>
       <div className='h-[35.125rem] w-[43.75rem] bg-white rounded-xl m-5 text-center shadow-xl flex justify-center items-center flex-col dark:bg-[#1e1e1e] dark:text-white'>
         <h1 className='font-bold text-3xl sm:text-4xl'>HEY, I'M KARTIK CHOUHAN</h1>
         <p className='text-sm sm:text-base'>A Frontend focused Web Developer building the Frontend of Web Applications.</p>
